3747aa0a8de8bc82586eac0c155a6b0854bc089c
Jayesh Krishna <jayeshkr152005@gmail.com>
2025-04-13 15:07:14 +0530

    Fixed issue

--- replay.py
+++ replay.py
@@ -4,8 +4,50 @@ import time
 from pynput.mouse import Controller as MouseController, Button
 from pynput.keyboard import Controller as KeyboardController, Key
 
+import ctypes
+user32 = ctypes.windll.user32
+current_width, current_height = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+
 mouse = MouseController()
 keyboard = KeyboardController()
+
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+
+KEY_MAPPING = {
+    'Key.enter': Key.enter,
+    'Key.space': Key.space,
+    'Key.shift': Key.shift,
+    'Key.ctrl': Key.ctrl,
+    'Key.alt': Key.alt,
+    'Key.cmd': Key.cmd,
+    'Key.esc': Key.esc,
+    'Key.tab': Key.tab,
+    'Key.backspace': Key.backspace,
+    'Key.delete': Key.delete,
+    'Key.up': Key.up,
+    'Key.down': Key.down,
+    'Key.left': Key.left,
+    'Key.right': Key.right,
+    'Key.home': Key.home,
+    'Key.end': Key.end,
+    'Key.page_up': Key.page_up,
+    'Key.page_down': Key.page_down,
+    'Key.insert': Key.insert,
+    'Key.menu': Key.menu,
+    'Key.f1': Key.f1,
+    'Key.f2': Key.f2,
+    'Key.f3': Key.f3,
+    'Key.f4': Key.f4,
+    'Key.f5': Key.f5,
+    'Key.f6': Key.f6,
+    'Key.f7': Key.f7,
+    'Key.f8': Key.f8,
+    'Key.f9': Key.f9,
+    'Key.f10': Key.f10,
+    'Key.f11': Key.f11,
+    'Key.f12': Key.f12,
+}
 
 if len(sys.argv) < 2:
     print("Please provide a JSON file to replay")
@@ -18,13 +60,16 @@ start_time = events[0]['timestamp']
 
 for event in events:
     delay = event['timestamp'] - start_time
-    time.sleep(delay)
+    if delay > 0 :
+        time.sleep(delay)
     start_time = event['timestamp']
 
     if event['type'] == 'mouse_move':
         details = event['details']
-        x = details['x']
-        y = details['y']
+        x_scale = current_width / SCREEN_WIDTH
+        y_scale = current_height / SCREEN_HEIGHT
+        x = details['x'] * x_scale
+        y = details['y'] * y_scale
         mouse.position = (x, y)
 
     elif event['type'] == 'mouse_click':
@@ -42,14 +87,27 @@ for event in events:
         mouse.scroll(event['dx'], event['dy'])
 
     elif event['type'] == 'key_press':
-        try:
-            details = event['details']
-            keyboard.press(details['key'])
-        except:
-            pass
+        details = event['details']
+        key = details['key']
+        
+        # Handle special keys
+        if key in KEY_MAPPING:
+            keyboard.press(KEY_MAPPING[key])
+        # Handle regular keys (remove quotes if present)
+        elif key.startswith("'") and key.endswith("'"):
+            keyboard.press(key[1:-1])
+        else:
+            keyboard.press(key)
+
     elif event['type'] == 'key_release':
-        try:
-            details = event['details']
-            keyboard.release(details['key'])
-        except:
-            pass
+        details = event['details']
+        key = details['key']
+        
+        # Handle special keys
+        if key in KEY_MAPPING:
+            keyboard.release(KEY_MAPPING[key])
+        # Handle regular keys (remove quotes if present)
+        elif key.startswith("'") and key.endswith("'"):
+            keyboard.release(key[1:-1])
+        else:
+            keyboard.release(key)
